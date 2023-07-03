3ac052e50702724d8e708d55a070f1d347598cf8
EC2 Default User <ec2-user@ip-172-31-8-216.eu-west-2.compute.internal>
2023-07-03 13:24:01 +0000

    initial commit

---
+++ OCP 17/Animal.java
@@ -0,0 +1,9 @@
+public class Animal{
+ public final static void main(final String... args){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
+class Animal2{
+   
+}
---
+++ OCP 17/TestClass.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+import java.util.Date;
+public class TestClass{
+    public static void main(String args[]){
+       Date d = new Date();
+        System.out.println(d);
+        
+    }
+}
